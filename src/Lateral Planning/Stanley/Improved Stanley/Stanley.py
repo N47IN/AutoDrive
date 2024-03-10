43f48204bbd26cf72457344573dd7d8bb2fc15ef
Navin <97350894+N47IN@users.noreply.github.com>
2024-03-10 12:09:08 +0530

    Update Stanley.py

--- src/Lateral Planning/Stanley/Improved Stanley/Stanley.py
+++ src/Lateral Planning/Stanley/Improved Stanley/Stanley.py
@@ -26,42 +26,44 @@ class LatControl():
         k = (self.path[self.i +1,1] - self.m*self.path[self.i +1,0] - self.c )/ math.sqrt(1+self.m**2)
         return k/abs(k)
     
-    def heading_error(self,curr_pos,yaw):     
+    def heading_error(self,curr_pos,yaw):
+        """ h_vecx = self.path[self.i,0] - self.path[self.i-1,0]
+        h_vecy = self.path[self.i,1] - self.path[self.i-1,1]
+        k = h_vecy/h_vecx
+        if k > 0:
+             if h_vecx > 0:
+                  self.path_angle = math.atan(k)
+             else:
+                  self.path_angle = math.pi + math.atan(k)
+        else:
+             if h_vecx < 0:
+                  self.path_angle = math.pi + math.atan(k)
+             else:
+                  self.path_angle = 2*math.pi + math.atan(k) """
+                          
         self.m = (self.path[self.i -1,1] - self.path[self.i,1])/(self.path[self.i -1,0] - self.path[self.i,0]) 
         self.path_angle = math.atan(self.m)
         if self.path_angle < 0:
-            self.path_angle = math.pi - self.path_angle
+            self.path_angle = math.pi + self.path_angle
         
-        if self.sign[self.i] > 0:
+        if self.i!=1 :
             self.path_angle = math.pi - self.path_angle
             #print("path",self.path_angle)
     
-        if abs(self.path_angle) < 0.2:
-            if yaw < 0:
-             print("case 1a")
-             yaw = yaw + math.pi
-             #print("yaw1",yaw)
-             e = -self.path_angle + yaw
-            else:
-             yaw = yaw - math.pi
-             print("case 1b")
-             #print("yaw1",yaw)
-             e = -self.path_angle + yaw
-        elif abs(math.pi - self.path_angle) < 0.2:   
-            yaw = yaw + math.pi
-            print("case 2")
-            e = -self.path_angle +yaw
-        else:
-            print("case 3")
-            if yaw < 0:
+        if yaw < 0:
                 yaw = math.pi + yaw
-            else:
+                e = self.path_angle - yaw
+                
+        else:
                 yaw = yaw - math.pi
-            e = self.path_angle - yaw
-
+                e = self.path_angle - yaw
+                
+        
+        
+        
         print("path",self.path_angle)
         print("heading correction",e)      
-        return e
+        return -e
     
     def steer_control(self,curr_pos,yaw,speed):
         K = 1.5
